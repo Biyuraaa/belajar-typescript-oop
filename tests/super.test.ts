c90fe9939ed17736707e427299503acc6413095e
M Bimo Bayu Bagaskara <m.bimo.bayu.bagaskara-2022@fst.unair.ac.id>
2024-09-18 15:49:41 +0700

    Super Constructor TypeScript

---
+++ tests/super.test.ts
@@ -0,0 +1,62 @@
+describe("Test Super", () => {
+  interface HasName {
+    name: string;
+  }
+
+  interface CanSayHello {
+    sayHello(name: string): string;
+  }
+
+  class Employee implements HasName, CanSayHello {
+    id: string;
+    _name?: string;
+
+    constructor(id: string, name?: string) {
+      this.id = id;
+      this._name = name;
+    }
+
+    get name(): string {
+      if (this._name) {
+        return this._name;
+      } else {
+        return "empty";
+      }
+    }
+
+    set name(name: string) {
+      if (name !== "") {
+        this._name = name;
+      }
+    }
+
+    sayHello(name: string): string {
+      return `Hello ${name}, my name is ${this.name}`;
+    }
+  }
+
+  class Manager extends Employee {
+    department: string;
+
+    constructor(name: string, department: string) {
+      super(name);
+      this.department = department;
+    }
+  }
+  class VicePresident extends Employee {}
+
+  it("should support super constructor in typescript", () => {
+    const employee = new Employee("187221049");
+
+    expect(employee.name).toBe("empty");
+
+    employee.name = "M Bimo Bayu Bagaskara";
+    expect(employee.name).toBe("M Bimo Bayu Bagaskara");
+    expect(employee.sayHello("Rizal")).toBe(
+      "Hello Rizal, my name is M Bimo Bayu Bagaskara"
+    );
+
+    const manager = new Manager("Biyuraaa", "IT");
+    expect(manager.department).toBe("IT");
+  });
+});
